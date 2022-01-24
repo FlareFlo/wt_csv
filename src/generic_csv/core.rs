2cfe7f0f84973e5c7b8500a85c2803580291b917
FlareFlo <florianlipp17@gmail.com>
2022-01-24 22:55:29 +0100

    Add lang files

--- src/generic_csv/core.rs
+++ src/generic_csv/core.rs
@@ -1,8 +1,11 @@
 use std::fmt::format;
 use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
+
+use crate::{DELIMITER, RECORD_SEP};
 use crate::generic_csv::header::Header;
 use crate::generic_csv::record::Record;
-use crate::{DELIMITER, RECORD_SEP};
 
 #[derive(Debug)]
 pub struct WTCSV {
@@ -16,18 +19,18 @@ impl WTCSV {
 	pub fn new_from_file(file: String) -> Result<Self, String> {
 		let header = Header::from_file(&file)?;
 
+
+
+		// iter.remove(0);
+		// iter.remove(iter.len() - 1);
+
 		let mut wtcsv = Self {
 			base_file: "".to_owned(),
 			header,
 			records: Vec::new(),
 		};
 
-		let mut iter = file.clone()
-			.split(RECORD_SEP).map(|x|x.to_owned()).collect::<Vec<String>>();
-		iter.remove(0);
-		iter.remove(iter.len() - 1);
-
-		for record in iter {
+		for record in records {
 			wtcsv.insert_record(&record)?;
 		}
 
@@ -43,7 +46,7 @@ impl WTCSV {
 			Ok(())
 		} else {
 			println!("{}", record);
-			Err(format!("Expected record length of {}, found actual length of {}",  self.header.len, serialized_record.items.len()))
+			Err(format!("Expected record length of {}, found actual length of {}", self.header.len, serialized_record.items.len()))
 		}
 	}
 }
@@ -58,10 +61,10 @@ mod tests {
 
 	#[test]
 	fn test_core_insert() {
-		let file = fs::read_to_string("lang/units.csv").unwrap();
+		let file = fs::read_to_string("lang/_common_languages.csv").unwrap();
 
 		let wtcsv = WTCSV::new_from_file(file).unwrap();
 
-		eprintln!("wtcsv = {:#?}", wtcsv);
+		eprintln!("wtcsv = {:#?}", wtcsv.records);
 	}
 }
