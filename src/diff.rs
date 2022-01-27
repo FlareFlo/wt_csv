55a8b0ec91823f63a2a5b4a470ebfb81bb7bf93f
FlareFlo <florianlipp17@gmail.com>
2022-01-27 18:45:58 +0100

    Added diff function

--- src/diff.rs
+++ src/diff.rs
@@ -1,5 +1,6 @@
 use crate::wtcsv::core::wtcsv::WTCSV;
 
+#[derive(Debug)]
 pub struct Diff {
 	pub id: String,
 	pub old: String,
@@ -7,9 +8,43 @@ pub struct Diff {
 }
 
 impl Diff {
-	pub fn diff_from_ref(left: &WTCSV, right: &WTCSV) -> Vec<Self> {
-		let diffs: Vec<Self> =  Vec::new();
+	pub fn diff_from_ref(left: &WTCSV, right: &WTCSV) -> Result<Vec<Self>, String> {
+		left.is_same(right)?;
 
-		vec![]
+		let mut diffs: Vec<Self> =  Vec::new();
+
+		for (i, record) in left.records.iter().enumerate() {
+			for (j, item) in record.items.iter().enumerate() {
+				if right.records[i].items[j] != *item {
+					diffs.push(Diff {
+						id: record.items[0].clone(),
+						old: item.clone(),
+						new: right.records[i].items[j].clone() ,
+					});
+					break;
+				}
+			}
+		}
+
+		Ok(diffs)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use std::fs;
+	use crate::diff::Diff;
+	use crate::wtcsv::core::wtcsv::WTCSV;
+
+	#[test]
+	fn expect_diff() {
+		let init = fs::read_to_string("lang/_common_languages.csv").unwrap();
+		let init = WTCSV::new_from_file(init).unwrap();
+
+		let mut diff = init.clone();
+		diff.edit_record_by_id("country_britain", "tea").unwrap();
+
+		let diff_res = Diff::diff_from_ref(&init, &diff).unwrap();
+		eprintln!("diff_res = {:?}", diff_res);
 	}
 }
--- src/wtcsv/core/compatibility.rs
+++ src/wtcsv/core/compatibility.rs
@@ -1,29 +1,45 @@
 use crate::wtcsv::core::wtcsv::WTCSV;
 
-/// Returns this enum when the two files are incompatible, the contents of enums always refer to other
+/// Returns this enum when the two files are incompatible, left is expected right is provided
 #[derive(Debug)]
 pub enum SameErr {
-	HeaderLen(usize),
-	HeaderContent(String),
-	RecordLength(usize),
+	HeaderLen(usize, usize),
+	HeaderContent(String, String),
+	RecordLength(usize, usize),
+}
+
+impl ToString for SameErr {
+	fn to_string(&self) -> String {
+		match self {
+			SameErr::HeaderLen(left, right) => {format!("Header Length mismatch, expected {left} but found {right}")}
+			SameErr::HeaderContent(left, right) => {format!("Header content mismatch, expected {left} but found {right}")}
+			SameErr::RecordLength(left, right) => {format!("Record content mismatch, expected {left} but found {right}")}
+		}
+	}
+}
+
+impl From<SameErr> for String {
+	fn from(err: SameErr) -> Self {
+		err.to_string()
+	}
 }
 
 impl WTCSV {
 	pub fn is_same(&self, other: &Self) -> Result<(), SameErr> {
 		// Checking header length first
 		if self.header.params.len() != other.header.params.len() {
-			return Err(SameErr::HeaderLen(other.header.params.len()));
+			return Err(SameErr::HeaderLen(self.header.params.len(), other.header.params.len()));
 		}
 
 		// Both CSVs must have an equal amount of records
 		if self.records.len() != other.records.len() {
-			return Err(SameErr::RecordLength(other.records.len()))
+			return Err(SameErr::RecordLength(self.records.len(), other.records.len()))
 		}
 
 		// Verifying if headers are matching and in order
 		for (i, param) in self.header.params.iter().enumerate() {
 			if other.header.params[i] != *param {
-				return Err(SameErr::HeaderContent(param.clone()));
+				return Err(SameErr::HeaderContent(param.to_owned(), other.header.params[i].to_owned()));
 			}
 		}
 
@@ -56,7 +72,7 @@ mod tests {
 		let wtcsv_lang = WTCSV::new_from_file(lang).unwrap();
 
 		match wtcsv_units.is_same(&wtcsv_lang) {
-			Err(SameErr::RecordLength(35)) => {
+			Err(SameErr::RecordLength(10432, 35)) => {
 			}
 			_ => {
 				panic!("See test for more details")
